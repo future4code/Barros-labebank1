ec4bb9f84f8af20a7e809c0fffd27c33c4bf9bd3
Bianca Paccola <bianca.paccola@gmail.com>
2022-11-13 17:18:03 -0300

    lógica para pegar a idade a partir da data de nascimento

--- labebank/src/index.ts
+++ labebank/src/index.ts
@@ -1,5 +1,5 @@
 console.log("LabeBank");
-import express from "express"
+import express, {Response, Request} from "express"
 
 import cors from 'cors'
 
@@ -9,6 +9,23 @@ app.use(express.json())
 
 app.use(cors())
 
+app.post('/users/createaccount', (req: Request, res: Response) => {
+    try{
+       const {name, CPF, birthDate} = req.body
+       let checkDate = birthDate.split('-').reverse().join('-')
+       let today = Date.now();
+       let birth: any = new Date(`${checkDate} 00:00:00`)
+       let year = 1000*60*60*24*365
+       let age = (today - birth) / year
+       age = Math.floor(age)
+       
+       console.log(age)
+       res.status(200).send(age);
+       
+    }catch(err) {
+        res.status(400).send("erro");
+    }
+});
 
 
 app.listen(3003, () => {
