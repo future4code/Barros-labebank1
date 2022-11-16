412639009c3897de8d6d2311101cac18ff75e13e
carolinemartins <caroline.martins.soares09@gmail.com>
2022-11-16 17:35:44 -0300

    Busca usuarios, buscar saldo

--- labebank/src/index.ts
+++ labebank/src/index.ts
@@ -1,57 +1,57 @@
 console.log("LabeBank");
-import express, {Response, Request} from "express";
+import express, { Response, Request, response } from "express";
 import cors from 'cors';
-import {account} from "./data";
+import { account } from "./data";
 
 const app = express()
 
 app.use(express.json())
 
 app.use(cors())
-
+//Criar conta ==>
 app.post('/users/createaccount', (req: Request, res: Response) => {
     let errorCode = 400;
-    try{
-        const {name, CPF, birthDate} = req.body
+    try {
+        const { name, CPF, birthDate } = req.body
 
-        if(!name || !CPF || !birthDate) {
-         errorCode = 422;
-         throw new Error("Obrigatóriamente deve informar: Nome completo, CPF válido e data de nascimento.")
+        if (!name || !CPF || !birthDate) {
+            errorCode = 422;
+            throw new Error("Obrigatóriamente deve informar: Nome completo, CPF válido e data de nascimento.")
         }
-       
-       // Cálculo para encontrar a idade a partir da data de nascimento 
-       let checkDate = birthDate.split('-').reverse().join('-')
-       let birth: any = new Date(`${checkDate} 00:00:00`)
-       let year = 1000*60*60*24*365
-       let age = Math.floor((Date.now() - birth) / year)
 
-       if (age < 18) {
-        errorCode = 403;
-        throw new Error("Necessário ter 18 anos completos para abrir uma conta.")
-       }
+        // Cálculo para encontrar a idade a partir da data de nascimento 
+        let checkDate = birthDate.split('-').reverse().join('-')
+        let birth: any = new Date(`${checkDate} 00:00:00`)
+        let year = 1000 * 60 * 60 * 24 * 365
+        let age = Math.floor((Date.now() - birth) / year)
 
-       const searchCPF = account.filter((user) => user.CPF === CPF);
+        if (age < 18) {
+            errorCode = 403;
+            throw new Error("Necessário ter 18 anos completos para abrir uma conta.")
+        }
 
-       if (searchCPF.length > 0) {
-        errorCode = 409;
-        throw new Error("CPF já cadastrado na base de dados. Só é permitido uma conta por CPF.")
-       }
-       
-       account.push({
-        name,
-        CPF,
-        birthDate,
-        saldo: 0,
-        extrato: []
-       })
+        const searchCPF = account.filter((user) => user.CPF === CPF);
 
-       res.status(201).send("Conta cadastrada com sucesso!");
-       
-    }catch(err: any) {
+        if (searchCPF.length > 0) {
+            errorCode = 409;
+            throw new Error("CPF já cadastrado na base de dados. Só é permitido uma conta por CPF.")
+        }
+
+        account.push({
+            name,
+            CPF,
+            birthDate,
+            saldo: 0,
+            extrato: []
+        })
+
+        res.status(201).send("Conta cadastrada com sucesso!");
+
+    } catch (err: any) {
         res.status(errorCode).send(err.message);
     }
 });
-
+// Transferência Interna ==>
 app.patch('/users/transfer', (req: Request, res: Response) => {
     let errorCode = 400;
     try {
@@ -66,7 +66,7 @@ app.patch('/users/transfer', (req: Request, res: Response) => {
             errorCode = 422
             throw new Error("Deve informar nome e CPF do pagador da transferência");
         }
-        
+
         if (!infoTransfer.receiver || !infoTransfer.receiver.name || !infoTransfer.receiver.CPF) {
             errorCode = 422
             throw new Error("Deve informar nome e CPF do recebedor da transferência");
@@ -92,7 +92,7 @@ app.patch('/users/transfer', (req: Request, res: Response) => {
 
         account[indexPayee].saldo -= infoTransfer.value
         account[indexReceiver].saldo += infoTransfer.value
-        
+
         res.status(201).send(`Transferência de R$ ${infoTransfer.value} enviada com sucesso!`);
 
     } catch (err: any) {
@@ -100,6 +100,54 @@ app.patch('/users/transfer', (req: Request, res: Response) => {
     }
 });
 
+//Buscar todos Usuarios ===>
+app.get("/users", (req: Request, resp: Response) => {
+    let errorCode = 400;
+    try {
+
+        const token = req.query.authorization;
+        if (!token) {
+            errorCode = 401
+            throw new Error("Erro de autenticação");
+        }
+        resp.status(200).send(account)
+
+    } catch (err: any) {
+        resp.status(errorCode).send(err.message)
+    }
+
+})
+
+//Pegar Saldo ==>
+
+app.get("/user/balance", (req: Request, resp: Response) => {
+    let errorCode = 400;
+    try {
+        const { name } = req.body
+        const { CPF } = req.body
+
+        let balanceUser
+        if (!name || !CPF) {
+            errorCode = 422;
+            throw new Error("Não foi passado o body corretamente");
+        }
+        const balance = account.filter((user) => {
+            if (user.CPF === CPF && user.name.toUpperCase() === name.toUpperCase()) {
+                balanceUser = user.saldo
+            }
+        }
+        )
+        if (!balance) {
+            errorCode = 404
+            throw new Error("Não encontrado");
+        }
+
+        resp.status(200).send(`Seu saldo é de R$: ${balanceUser}`);
+
+    } catch (err: any) {
+        resp.status(errorCode).send(err.message)
+    }
+})
 
 app.listen(3003, () => {
     console.log("Server is running in http://localhost:3003");
